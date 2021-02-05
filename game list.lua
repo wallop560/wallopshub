98e145f5019cd9895e064d3e757a661da6891dcf
wallop560 <72463875+wallop560@users.noreply.github.com>
2021-02-05 15:28:52 +0000

    Create game list.lua

---
+++ game list.lua
@@ -0,0 +1,8 @@
+{
+    "13822889": {
+        "ScriptLink": "https://raw.githubusercontent.com/wallop560/wallopshub/main/lumber%20tycoon2.lua"
+    },
+    "5977280685": {
+        "ScriptLink": "https://raw.githubusercontent.com/wallop560/wallopshub/main/ninja%20legends2.lua"
+    }
+}
