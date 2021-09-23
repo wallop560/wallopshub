1c3747d1e3d4f5e054da564df710d5bd9ce5b142
wallop560 <72463875+wallop560@users.noreply.github.com>
2021-09-23 22:03:22 +0100

    Update game list.lua

--- game list.lua
+++ game list.lua
@@ -1,6 +1,6 @@
 {
     "13822889": {
-        "ScriptLink": "https://raw.githubusercontent.com/wallop560/wallopshub/main/Ver3.lua"
+        "ScriptLink": "https://raw.githubusercontent.com/wallop560/wallopshub/main/Ver3"
     },
     "5977280685": {
         "ScriptLink": "https://raw.githubusercontent.com/wallop560/wallopshub/main/ninja%20legends2.lua"
