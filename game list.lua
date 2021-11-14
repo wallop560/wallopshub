7294f686c714008f4740346562733fa2a09f02f6
wallop560 <72463875+wallop560@users.noreply.github.com>
2021-11-14 14:55:47 +0000

    Update game list.lua

--- game list.lua
+++ game list.lua
@@ -1,7 +1,7 @@
 {
     "13822889": {
         "ScriptLink": "https://raw.githubusercontent.com/wallop560/wallopshub/main/Ver3",
-        "GameName": "LumberTycoon2"
+        "GameName": "Lumber Tycoon2"
     },
     "5977280685": {
         "ScriptLink": "https://raw.githubusercontent.com/wallop560/wallopshub/main/ninja%20legends2.lua",
